ac3926f8b9d06a45e4ff1fda954e2000d9ef829b
Richard Moreano <ri.moreano@duocuc.cl>
2025-05-14 20:43:48 -0400

    Creacion de algunos modelos

--- src/main/java/com/edutech/micros/edutech/repository/UsuarioRepository.java
+++ src/main/java/com/edutech/micros/edutech/repository/UsuarioRepository.java
@@ -1,4 +1,10 @@
 package com.edutech.micros.edutech.repository;
 
-public interface UsuarioRepository {
+import com.edutech.micros.edutech.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    List<Usuario> findAll();
 }
--- src/main/java/com/edutech/micros/edutech/service/UsuarioService.java
+++ src/main/java/com/edutech/micros/edutech/service/UsuarioService.java
@@ -1,4 +1,32 @@
 package com.edutech.micros.edutech.service;
 
+import com.edutech.micros.edutech.model.Usuario;
+import com.edutech.micros.edutech.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> findAll(){
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario findById(long id){
+        return usuarioRepository.findById(id).get();
+    }
+
+    public Usuario save(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    public void delete(long id){
+        usuarioRepository.deleteById(id);
+    }
 }
+
