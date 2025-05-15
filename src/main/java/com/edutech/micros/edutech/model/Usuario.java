d585ce876b07980d48766125c32f214e1e0dc3e9
Richard Moreano <ri.moreano@duocuc.cl>
2025-05-14 20:35:01 -0400

    Creacion de algunos modelos

--- src/main/java/com/edutech/micros/edutech/model/Usuario.java
+++ src/main/java/com/edutech/micros/edutech/model/Usuario.java
@@ -1,5 +1,35 @@
 package com.edutech.micros.edutech.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
-    hola
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 13, nullable = false)
+    private String run;
+
+    @Column(length = 100, nullable = false)
+    private String nombre;
+
+    @Column(length = 100, nullable = false)
+    private String apellido;
+
+    @Column(length = 100, nullable = false)
+    private String correo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pago_fk", nullable = false)
+    private Pago pago;
+
 }
+
