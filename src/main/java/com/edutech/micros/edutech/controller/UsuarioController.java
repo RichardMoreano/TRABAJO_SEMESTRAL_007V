b9a18f3d00d25fa83d58cd7738869cfad0bf5fab
Ghost <s21243004@gmail.com>
2025-05-17 13:47:53 -0400

    prueba

--- src/main/java/com/edutech/micros/edutech/controller/UsuarioController.java
+++ src/main/java/com/edutech/micros/edutech/controller/UsuarioController.java
@@ -1,4 +1,10 @@
 package com.edutech.micros.edutech.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/usuario")
 public class UsuarioController {
+
 }
