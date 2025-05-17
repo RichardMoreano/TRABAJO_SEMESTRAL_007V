1789285d4b61ba3aa036d72d35bef855d2f45986
domiu <do.cofre@duocuc.cl>
2025-05-17 14:19:45 -0400

    se agregaron nuevos modelos y sus atributos

--- src/main/java/com/edutech/micros/edutech/model/Usuario.java
+++ src/main/java/com/edutech/micros/edutech/model/Usuario.java
@@ -14,18 +14,20 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+// se genera un id unico para cada usuario
     @Column(unique = true, length = 13, nullable = false)
     private String run;
-
+// el unique hace que no se pueda generar en este caso otro run y el nullable es para que de nulo si ya esta un dato
     @Column(length = 100, nullable = false)
     private String nombre;
+// length para el largo del dato
 
     @Column(length = 100, nullable = false)
     private String apellido;
 
-    @Column(length = 100, nullable = false)
+    @Column(unique = true, length = 100, nullable = false)
     private String correo;
+// conecta usuario con pedido con la foreign key id de pedido llama todos los atributos
 
     @ManyToOne
     @JoinColumn(name = "id_pedido_fk", nullable = false)
