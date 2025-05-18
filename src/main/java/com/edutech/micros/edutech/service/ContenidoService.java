cb48f6af4fbe6c11be14c323c8161faa2f68bf66
willi <wi.osorio@duocuc.cl>
2025-05-18 15:15:36 -0400

    Error corregido en ContenidoService

--- src/main/java/com/edutech/micros/edutech/service/ContenidoService.java
+++ src/main/java/com/edutech/micros/edutech/service/ContenidoService.java
@@ -5,7 +5,7 @@ import com.edutech.micros.edutech.repository.ContenidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class ContenidoService {
