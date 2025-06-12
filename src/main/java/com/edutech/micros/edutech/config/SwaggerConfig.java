2a398309f689fb1d22586a699b5f4a25e427816f
domiu <do.cofre@duocuc.cl>
2025-06-11 21:34:03 -0400

    se agrego el paquete configuracion de Swagger

--- src/main/java/com/edutech/micros/edutech/config/SwaggerConfig.java
+++ src/main/java/com/edutech/micros/edutech/config/SwaggerConfig.java
@@ -1,4 +1,21 @@
 package com.edutech.micros.edutech.config;
 
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI costumOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                .title("API 2025 EDUTECH")
+                .version("1.0")
+                .description("documentqacion de la API para el sistema EDUTECH"));
+
+
+    }
 }
