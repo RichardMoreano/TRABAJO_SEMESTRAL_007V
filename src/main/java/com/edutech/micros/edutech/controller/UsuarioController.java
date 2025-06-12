3c77b18fd44174734575b2df855fd88f12f67dbe
domiu <do.cofre@duocuc.cl>
2025-06-11 21:55:34 -0400

    se agregaron algunas anotaciones swagger en el controller de usuario

--- src/main/java/com/edutech/micros/edutech/controller/UsuarioController.java
+++ src/main/java/com/edutech/micros/edutech/controller/UsuarioController.java
@@ -3,6 +3,8 @@ package com.edutech.micros.edutech.controller;
 import com.edutech.micros.edutech.model.Usuario;
 import com.edutech.micros.edutech.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +21,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuarios")
+
+@Tag(name = "Usuarios", description = "operaciones relacionadas con la gestion de usuarios")
 public class UsuarioController {
 
 //se utiliza para hacer inyección de dependencias
@@ -30,11 +34,13 @@ public class UsuarioController {
     }
 
     @GetMapping
+   @Operation(summary ="obtener todos los usuarios",description = "obtiene una lista con los usuarios")
     public List<Usuario> findAll() {
         return usuarioService.findAll();
     }
 
     @PostMapping
+    @Operation(summary = "guarda una usuario ", description = "guarda una usuario completamente nuevo")
     public Usuario postUsuario(@RequestBody Usuario usuario) {
         return usuarioService.save(usuario);
     }
@@ -45,6 +51,7 @@ public class UsuarioController {
     }*/
 
     @GetMapping("/{id}")
+    @Operation(summary = "buscar por id", description = "busca por el id del usuario en la base de datos")
     public ResponseEntity<?> findUsuario(@PathVariable Long id) {
         Usuario usuario = usuarioService.findByIdusuario(id);
         if (usuario == null) {
@@ -61,6 +68,7 @@ public class UsuarioController {
     }*/
 
     @GetMapping("/correo/{correo}")
+    @Operation(summary = "busca por correo", description = "busca por el correo del usuario en la base de datos")
     public ResponseEntity<?> findCorreo(@PathVariable String correo) {
         List<Usuario> usuario = usuarioService.findByCorreo(correo);
         if (usuario.isEmpty()) {
@@ -73,16 +81,19 @@ public class UsuarioController {
     }
 
     @GetMapping("/nombre/{nombre}/{apellido}")
+    @Operation(summary = "buscar por el nombre completo", description = "busca por el nombre completo del usuario en la base de datos")
     public List<Usuario> findNombreCompleto(@PathVariable String nombre, @PathVariable String apellido) {
         return usuarioService.findByNombreCompleto(nombre, apellido);
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "eliminar por id", description = "elimina toda la informacion por el id del usuario")
     public void deleteUsuario(@PathVariable Long id) {
         usuarioService.deleteUsuario(id);
     }
 
     @PutMapping("/actua")
+    @Operation(summary = "actualizar los datos", description = "actualiza los datos del usuario menos el id ")
     public Usuario updateUsuario(@RequestBody Usuario usuario) {
         return usuarioService.updateUsuario(usuario); // Usa save() para crear o actualizar
     }
