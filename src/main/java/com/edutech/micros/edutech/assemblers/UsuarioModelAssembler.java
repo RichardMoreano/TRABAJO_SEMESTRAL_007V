53aab336880c1fb8740fec10bff11b84871838b7
domiu <do.cofre@duocuc.cl>
2025-06-18 20:37:00 -0400

    se han añadido dos nuevas clases en los siguentes packages: Assembler y controller de los siguentes microservicios: usuario y reporte

---
+++ src/main/java/com/edutech/micros/edutech/assemblers/UsuarioModelAssembler.java
@@ -0,0 +1,12 @@
+package com.edutech.micros.edutech.assemblers;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import com.edutech.micros.edutech.controller.UsuarioController;
+import com.edutech.micros.edutech.model.Usuario;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UsuarioModelAssembler {
+}
